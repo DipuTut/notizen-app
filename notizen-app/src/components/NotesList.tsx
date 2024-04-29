394e8c6b2a5354864188dd2e3b9eba4c300ef801
DipuTut <mddipu@gmail.com>
2024-04-29 13:07:19 +0200

    Fixing the submit button type.

--- notizen-app/src/components/NotesList.tsx
+++ notizen-app/src/components/NotesList.tsx
@@ -1,9 +1,10 @@
-import NotesCard from "./Card";
+import NotesCard from "./NotesCard";
 import { Note } from "../types/notes.type";
 import { Col, Container, Row } from "react-bootstrap";
 
 type Props = {
-    notes: Note[]
+    notes: Note[],
+    setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 
 
@@ -13,23 +14,20 @@ function NotesList(props: Props){
         <>
             <Container>
                 <Row>
-                   
                     {
                         props.notes.map(note => 
-                        <Col md={3} key={note.id}>
-                        <NotesCard
-                        id={note.id}
-                        title={note.title}
-                        categories={note.categories}
-                        content={note.content} />)
-                        </Col>
-                   ) }
-                    
+                            <Col md={6} key={note.id}>
+                                <NotesCard
+                                id={note.id}
+                                title={note.title}
+                                categories={note.categories}
+                                content={note.content} />
+                            </Col>
+                    )}
                 </Row>
             </Container>
         </>
     )
-
 }
 
 export default NotesList;
