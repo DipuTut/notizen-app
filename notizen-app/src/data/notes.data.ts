ca976780e0ab1bab58a183a98942d0af173434be
DipuTut <mddipu@gmail.com>
2024-04-30 10:14:29 +0200

    Adding new fields date and user to the Note.

--- notizen-app/src/data/notes.data.ts
+++ notizen-app/src/data/notes.data.ts
@@ -6,21 +6,29 @@ export const notesData: Note[] = [
       title: 'Überschrift 1',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Büro', 'Haushalt'],
+      user: 'Asadul',
+      date: new Date(Date.now() - 36*1000),
       id: 1
     }, {
       title: 'Überschrift 2',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Büro'],
+      user: 'Lisa',
+      date: new Date(Date.now() - 24*36*1000),
       id: 2
     }, {
       title: 'Überschrift 3',
       content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
       categories: ['Haushalt'],
+      user: 'Lisa',
+      date: new Date(Date.now() - 48*36*1000),
       id: 3
     }, {
       title: 'Überschrift 4',
       content: 'Kurzer Text',
-      categories: [],
+      categories: ['Freizeit'],
+      user: 'Lisa',
+      date: new Date(Date.now() - 72*36*1000),
       id: 4
     }
   ]
